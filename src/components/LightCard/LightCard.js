0f02368ea72f440e0778ac03ae8f6a1e71df8d97
nkean <nathankean2@gmail.com>
2018-06-07 09:20:25 -0500

    Display each light with an on/off button

---
+++ src/components/LightCard/LightCard.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Card, Icon } from 'antd';
+
+const { Meta } = Card;
+
+class LightCard extends Component {
+
+  lightOn = () => {
+    console.log('light on');
+    const url = `http://${this.props.bridgeIP}/api/${this.props.hueToken}/lights/${this.props.light}/state`;
+    axios.put(url, {"on": true})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  lightOff = () => {
+    console.log('light off');
+    const url = `http://${this.props.bridgeIP}/api/${this.props.hueToken}/lights/${this.props.light}/state`;
+    axios.put(url, {"on": false})
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <Card
+          style={{width: 250}}
+          actions={[<span onClick={() => this.lightOn()}>ON</span>, <span onClick={() => this.lightOff()}>OFF</span>]}
+        >
+          <Meta
+            title={this.props.light}
+            description="This is a light"
+          />
+        </Card>
+      </div>
+    );
+  }
+}
+
+// this allows us to use <App /> in index.js
+export default LightCard;
